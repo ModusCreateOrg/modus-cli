0246c6b3c710cc06fc04d744273e7a0133ff39ce
Michael Schwartz <mykesx@gmail.com>
2018-05-30 09:11:49 -0700

    fix download of actual blob content

--- plugins/guidelines.js
+++ plugins/guidelines.js
@@ -55,7 +55,8 @@ class Guidelines {
   }
 
   async action(args) {
-    const {repo, overwrite, dest, samples} = args;
+    const {repo, overwrite, dest, samples} = args,
+          [org, repository, branc] = repo.split('/');
 
     this.stats = {
       overwritten: 0,
@@ -89,9 +90,9 @@ class Guidelines {
           continue;
         }
         if (node.type !== 'tree') {
-          const output = await this.safeFilename(path.join(dest, node.path), overwrite);
-          //          console.log('download(' + node.url + ',' + output + ');');
-          const downloader = new Downloader(node.url, output, node.mode);
+          const input = `https://raw.githubusercontent.com/${repo}/${node.path}`,
+                output = await this.safeFilename(path.join(dest, node.path), overwrite);
+          const downloader = new Downloader(input, output, node.mode);
           await downloader.download();
           this.stats.downloads++;
         }
